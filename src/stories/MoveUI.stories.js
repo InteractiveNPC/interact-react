0fe6508df6e2e8d137c169a112176ec0f68773b6
HeoJiye <heoze1031@gmail.com>
2023-03-14 11:33:55 +0900

    feat: 장소 이동 구현

--- src/stories/MoveUI.stories.js
+++ src/stories/MoveUI.stories.js
@@ -8,6 +8,44 @@ export default {
   component: MoveUI,
 };
 
+const props = {
+  locations: ["연못", "수사실", "나무꾼의 집"],
+};
+
+class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      locations: [
+        <div
+          style={{ width: "100%", height: "100%", background: "red" }}
+        ></div>,
+        <div
+          style={{ width: "100%", height: "100%", background: "blue" }}
+        ></div>,
+        <div
+          style={{ width: "100%", height: "100%", background: "green" }}
+        ></div>,
+      ],
+      locationNames: ["연못", "수사실", "나무꾼의 집"],
+      idx: 0,
+    };
+  }
+
+  render() {
+    return (
+      <div style={{ width: "100%", height: "100%" }}>
+        {this.state.locations[this.state.idx]}
+        <MoveUI
+          {...this.state}
+          onchange={(idx) => this.setState({ ...this.state, idx: idx })}
+        />
+      </div>
+    );
+  }
+}
+
 export const test = {
-  render: () => getWithFullscreen(<MoveUI />),
+  render: () => getWithFullscreen(<Example />),
 };
