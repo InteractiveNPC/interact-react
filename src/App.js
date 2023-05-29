60affe0af8f79590c93e86a52757bb317c21be81
Knock <chein1010@naver.com>
2023-05-30 00:49:01 +0900

    home: send result to home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Intro from "./components/IntroUI/Intro";
+import Home from "./components/HomeUI/Home";
 import { getResizeEventListener } from "./services/responsiveFrame";
 
 class App extends Component {
   render() {
     return (
       <div id="App">
-        <Intro />
+        <Home init="true"/>
       </div>
     );
   }
