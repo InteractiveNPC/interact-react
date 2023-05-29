f40fa58e283634fad2bbb679b9561db547bf70f3
Knock <chein1010@naver.com>
2023-05-30 00:49:01 +0900

    home: send result to home

--- src/components/IntroUI/Intro.js
+++ src/components/IntroUI/Intro.js
@@ -48,11 +48,11 @@ function Intro(props) {
           <div id="start">무원(無寃) 이란 '원통함, 억울함이 없게 하다'라는 뜻으로,<br/>원통한 일이 없도록 사건수사에 도움이 되는 법의학적 실무지식을 수집, 정리한 책이 '무원록'입니다.<br/>전래동화 속 원통한 일을 겪은 인물들을 만나 수사를 진행하며 당신만의 무원록을 완성하십시오.</div>
           <div id="info">* 본 컨텐츠는 전래동화 속 범죄를 수사하는 컨텐츠로,<br/>기존의 전래동화를 바탕으로 일부의 상상력을 보태어 만들어졌으며, 수사에 있어서 검찰의 공식적인 절차와 다를 수 있음을 알려드립니다.</div>
           <video preload="auto">
-            <source type="video/mp4" src="image/intro/title.mp4"/>
+            <source type="video/mp4" src="image/Intro/title.mp4"/>
           </video>
           <div id="begin">클릭하여 시작하세요</div>
         </div>
-      ) : <Home /> }
+      ) : <Home init="true"/> }
     </div>
   )
 
