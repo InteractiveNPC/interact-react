4a443403e8e53cc610362122c7ebcf020695bbdd
HeoJiye <heoze1031@gmail.com>
2023-05-30 17:37:05 +0900

    기존 App과 연결해둠

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 
-import App from "./router";
+import App from "./App";
 
 import "./styles/reset.css";
 
