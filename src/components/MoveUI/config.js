6b68237617154e890e041af65c91afc754265d05
HeoJiye <heoze1031@gmail.com>
2023-05-31 23:35:42 +0900

    chapter 나무꾼 = 1, 장화홍련 = 3 통일

--- src/components/MoveUI/config.js
+++ src/components/MoveUI/config.js
@@ -3,10 +3,10 @@ import twosistersLocs from "./twosisters";
 
 export const locationNames = {
   1: ["수사실", "나무꾼의 집", "나무꾼의 방", "연못", "천계"],
-  2: ["수사실", "장화홍련의 집", "연못", "관아 밖", "관아 안"],
+  3: ["수사실", "장화홍련의 집", "연못", "관아 밖", "관아 안"],
 };
 
 export const locations = {
   1: woodcutterLocs,
-  2: twosistersLocs,
+  3: twosistersLocs,
 };
