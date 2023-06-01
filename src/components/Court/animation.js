737fab32c8c8be92da274f3b3fa42181d44ae605
HeoJiye <heoze1031@gmail.com>
2023-06-01 15:57:44 +0900

    update court image

---
+++ src/components/Court/animation.js
@@ -0,0 +1,14 @@
+export const setButtonEvent = (target, src) => {
+  target.onmousedown = () =>
+    (target.style.backgroundImage = `url(${
+      process.env.PUBLIC_URL + src
+    }_click.png)`);
+  target.onmouseup = () =>
+    (target.style.backgroundImage = `url(${
+      process.env.PUBLIC_URL + src
+    }_normal.png)`);
+
+  target.style.backgroundImage = `url(${
+    process.env.PUBLIC_URL + src
+  }_normal.png)`;
+};
