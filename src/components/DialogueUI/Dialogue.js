cf676b81d48f0552f428dd99b398ef8aa89e5c22
Knock <chein1010@naver.com>
2023-06-01 21:46:13 +0900

    dialogue: run test

--- src/components/DialogueUI/Dialogue.js
+++ src/components/DialogueUI/Dialogue.js
@@ -74,13 +74,11 @@ function Dialogue(props) {
       if(res.data.chapter == -1) {
         $("#dialogue").off("click").on("click", {code: -1}, clickHandler);
       }
-      /*
       if(size <= index) {
         if(res.data.scene == end) {
           $("#dialogue").off("click").on("click", {code: 0}, clickHandler);
         }
       }
-      */
     })
     .catch(error => console.log(error))
   }
@@ -99,10 +97,6 @@ function Dialogue(props) {
 
   const answerId = (idx) => {
     return "answer" + idx;
-  }
-
-  if($("#dialogue_container").css("display") == "none") {
-    $("#dialogue_container").fadeIn(1000);
   }
 
   if (data.choice == null) {
