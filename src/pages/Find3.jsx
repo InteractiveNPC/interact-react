3b19c8205b7b05efeaf7cf9a341ba3964e17d222
HeoYuJinn <uwoorii@gmail.com>
2023-06-03 23:52:01 +0900

    20230603 error 해결용 commit

--- src/pages/Find3.jsx
+++ src/pages/Find3.jsx
@@ -35,7 +35,6 @@ export default function Find3(props) {
                     }} onMouseLeave={()=>{
                             $('img#shine').removeClass("shining");
                     }} onClick={()=>{
-                        {props.goOffice}
                         $('img#shine').addClass('shining2');
                         $('img#handle').fadeOut({fadeT});
                         setTimeout(()=>{
