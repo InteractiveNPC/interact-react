13f79d5b4339cc6310384e2d1a082ff8bf1cdf9d
HeoYuJinn <uwoorii@gmail.com>
2023-06-03 22:50:24 +0900

    20230603 conflict 해결을 위해 app.js 수정...

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
-import Dialogue from "./components/DialogueUI/Dialogue";
+import Intro from "./components/IntroUI/Intro";
 import Indict from "./components/Indict/index";
 import Indict2 from "./components/Indict/index2";
 import Indict3 from "./components/Indict/index3";
 import Tw1 from "./components/Indict/twsister1";
 import Tw2 from "./components/Indict/twosister2";
 import Tw3 from "./components/Indict/twosister3";
-import Mvui from "./components/MoveUI/index";
-
-import Intro from "./components/IntroUI/Intro";
-
 import { getResizeEventListener } from "./services/responsiveFrame";
 
 import Chapter from "./pages/chapter";
@@ -18,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div id="App">
-        <Tw3 />
+        <Intro />
       </div>
     );
   }
