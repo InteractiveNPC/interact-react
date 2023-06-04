a8c0c792fd7db3a59f11d11340d1a8b31a30121f
Knock <chein1010@naver.com>
2023-06-05 01:31:16 +0900

    resource: fix path issue

--- src/components/MoveUI/woodcutter.jsx
+++ src/components/MoveUI/woodcutter.jsx
@@ -6,13 +6,13 @@ import Bhelp from '../../pages/Help';
 import styles from "./style.module.scss";
 
 const BackgroundImgBase =
-  "/image/investigation/Talk/Background/FairyNWoodcutter/";
+  "/image/Investigation/Talk/Background/FairyNWoodcutter/";
 const fullWebpBase =
   process.env.PUBLIC_URL +
-  "/image/investigation/Talk/Source/FairyNWoodcutter/full/full_";
+  "/image/Investigation/Talk/Source/FairyNWoodcutter/full/full_";
 const halfWebpBase = 
   process.env.PUBLIC_URL +
-  "/image/investigation/Talk/Source/FairyNWoodcutter/half/half_";
+  "/image/Investigation/Talk/Source/FairyNWoodcutter/half/half_";
 const setPos = (x, y) => {
   return { style: { left: `${x}px`, top: `${y}px` } };
 };
@@ -40,7 +40,7 @@ export default [
           <div>
           <img
             src={halfWebpBase + "Fairy_normal_X_office.webp"}
-            {...setPosWithIdx(0, 0, 1000)}
+            {...setPosWithIdx(0, 0, 1111)}
             onClick={() => {
               onTalk({ idx: "1", scene: "-1", "flag": "0", index: "0" });
             }}
