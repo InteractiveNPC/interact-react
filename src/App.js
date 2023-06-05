45df9d3c081834e479c82af44028258d76e32fbb
Azamman327 <wjsdbdud327@naver.com>
2023-06-06 00:16:41 +0900

    conflict 방지

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React, { Component } from "react";
-import Intro from "./components/IntroUI/Intro";
-import Home from "./components/HomeUI/Home";
+import { useState, useEffect } from "react";
+import { VolumeContext } from "./contexts";
 import { getResizeEventListener } from "./services/responsiveFrame";
-import Chapter from "./pages/chapter";
+
+import Intro from "./components/IntroUI/Intro";
 
 import Loading from "./components/Loading";
-import PResult from "./pages/indict_result"
 
-class App extends Component {
-  render() {
-    return (
-      <div id="App">
-        <PResult />
-        {/* <Loading App={<Intro/>} /> */}
-      </div>
-    );
-  }
+export default () => {
+  const [volume, setVolume] = useState([0.5, 0.5, 0.5]);
 
-  componentDidMount() {
+  useEffect(()=>{
     const FixRatio = getResizeEventListener(1920, 1080);
     window.onresize = FixRatio;
     FixRatio();
-    //imagePreloader();
-  }
-  
-}
+  });
 
-export default App;
+   return (
+      <div id="App">
+        <VolumeContext.Provider value={[volume, setVolume]}>
+          <Loading App={<Intro/>} />
+        </VolumeContext.Provider>
+      </div>
+    );
+};
