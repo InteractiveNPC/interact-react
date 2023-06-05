0726db65afd206e767870d5edcff0e796ff4129e
HeoJiye <heoze1031@gmail.com>
2023-06-05 17:06:09 +0900

    사건 노트 노드 읽음 여부 & 읽음 체크 함수 추가

--- src/components/Record/hook.js
+++ src/components/Record/hook.js
@@ -1,37 +1,54 @@
 import axios from "axios";
 
-// 중심 노드 얻기
-/* [ {name, chapter, id, content, image, place, visited(노드 읽었는지 여부)}, ...] */
+// getMainNodes와 getNodes는 async 함수 내에서 await으로 값을 기다려서 받기 (스프링에서 값을 가져오는 시간)
+/* example
+    useEffect(async () => {
+        const mainNodes = await getMainNodes();
+        setMainNodes(mainNodes);
+    });
+*/
+
+// 중심 노드 정보 얻기
+/* return [ {name, chapter, id, content, image, place, **have(해당 노드 얻었는지 여부), visited(노드 읽었는지 여부)**}, ...] */
 export const getMainNodes = async (chapter) => {
     const have_nodeIds =  await getMeets(chapter);
-    const all_nodes = await mappingMainNodesInfo(chapter);
+    const visited_nodeIds = await getVisitMeets(chapter);
+    const all_nodes = await getMainNodesInfo(chapter);
 
-    const nodes = [];
-    for(let i of have_nodeIds) {
-        for(let j = 0; j < all_nodes.length; j++) {
-            if(i === all_nodes[j].id) {
-                nodes.push({...all_nodes[j], visited: false});
-            }
-        } 
-    }
-    return nodes.sort((a, b) => a.id - b.id);
-};
+    return mappingDatas(have_nodeIds, visited_nodeIds, all_nodes);
+};  
 
-// 주변 노드 얻기
-/* [ {name, kind, chapter, id, content, visited(노드 읽었는지 여부)}, ...] */
+// 주변 노드 정보 얻기
+/* return [ {name, kind, chapter, id, content, **have(해당 노드 얻었는지 여부), visited(노드 읽었는지 여부)**}, ...] */
 export const getNodes = async (chapter) => {
     const have_nodeIds =  await getItems(chapter);
-    const all_nodes = await mappingNodesInfo(chapter);
+    const visited_nodeIds = await getVisitItems(chapter);
+    const all_nodes = await getNodesInfo(chapter);
 
-    const nodes = [];
-    for(let i of have_nodeIds) {
-        for(let j = 0; j < all_nodes.length; j++) {
-            if(i === all_nodes[j].id) {
-                nodes.push({...all_nodes[j], visited: false});
-            }
-        } 
-    }
-    return nodes.sort((a, b) => a.id - b.id);
+    return mappingDatas(have_nodeIds, visited_nodeIds, all_nodes);
+}
+
+// 중심 노드 읽음 저장
+export const visitMainNode = (chapter, id) => {
+    axios
+    .get(`node/meet/${chapter}/${id}`)
+    .then((res) => {
+        console.log("챕터 " + chapter + "의 중심 노드 " + id + "획득!!");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+// 주변 노드 읽음 저장
+export const visitNode = (chapter, id) => {
+    axios
+    .get(`node/item/${chapter}/${id}`)
+    .then((res) => {
+        console.log("챕터 " + chapter + "의 주변 노드 " + id + "획득!!");
+    })
+    .catch((error) => {
+        console.log(error);
+    });    
 }
 
 const getMeets = (chapter) => {
@@ -60,7 +77,7 @@ const getItems = (chapter) => {
         })
     );
 };
-const mappingMainNodesInfo = (chapter) => {
+const getMainNodesInfo = (chapter) => {
     return new Promise((resolve) =>
         axios
         .get(`/item/getCharacters/${chapter}`)
@@ -73,7 +90,7 @@ const mappingMainNodesInfo = (chapter) => {
         })
     );
 };
-const mappingNodesInfo = (chapter) => {
+const getNodesInfo = (chapter) => {
     return new Promise((resolve) =>
         axios
         .get(`/item/getItems/${chapter}`)
@@ -86,3 +103,67 @@ const mappingNodesInfo = (chapter) => {
         })
     );
 };
+const getVisitMeets = (chapter) => {
+    return new Promise((resolve) =>
+        axios
+        .get(`/node/meet/${chapter}`)
+        .then(({ data }) => {
+            resolve(data);
+        })
+        .catch((error) => {
+            console.log(error);
+            resolve();
+        })
+    );
+};
+const getVisitItems = (chapter) => {
+    return new Promise((resolve) =>
+        axios
+        .get(`/node/item/${chapter}`)
+        .then(({ data }) => {
+            resolve(data);
+        })
+        .catch((error) => {
+            console.log(error);
+            resolve();
+        })
+    );
+};
+
+const mappingMainNodes = (have_nodeIds, visited_nodeIds, all_nodes) => {
+    const nodes = [];
+    for(let i of have_nodeIds) {
+        let visited = false;
+        for(let j of visited_nodeIds) {
+            if(i === j) {
+                visited = true;
+                break;
+            }
+        }
+        for(let j = 0; j < all_nodes.length; j++) {
+            if(i === all_nodes[j].id) {
+                nodes.push({...all_nodes[j], visited: visited});
+            }
+        } 
+    }
+    return nodes.sort((a, b) => a.id - b.id);   
+}
+const mappingDatas = (have_nodeIds, visited_nodeIds, all_nodes) => {
+    for(let node of all_nodes) {
+        node.have = false;
+        node.visited = false;
+
+        for(let i of have_nodeIds) {
+            if(i != node.id) continue;
+            node.have = true;
+
+            for(let j of visited_nodeIds) {
+                if(i != j) continue;
+                node.visited = true;
+                break;
+            }
+            break;
+        }
+    }
+    return all_nodes;   
+}
