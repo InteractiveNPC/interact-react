6b81862594c2f36ebcb02d8eab50b981b4728884
Fornamin <97093507+Fornamin@users.noreply.github.com>
2023-06-07 04:13:29 +0900

    .

--- src/pages/eventRecord2.jsx
+++ src/pages/eventRecord2.jsx
@@ -6,6 +6,10 @@ import { getMainNodes, getNodes, visitMainNode, visitNode } from "components/Rec
 export default function Record(){
     const background = 'image/Record/Background/Background.png'
 
+    const nameUI = 'image/Record/UI/name_ui.png';
+    const tape = 'image/Record/UI/tape.png';
+    const pin = 'image/Record/UI/FairyNWoodcutter/pin.png';
+
     const node1 = ['image/Record/Source/TwoSisters/Corpsepaper_node_dark.png', 'image/Record/Source/TwoSisters/Corpsepaper_node_glow.png', 'image/Record/Source/TwoSisters/Corpsepaper_node_normal.png']
     const node2 = ['image/Record/Source/TwoSisters/Hairpin_node_dark.png', 'image/Record/Source/TwoSisters/Hairpin_node_glow.png', 'image/Record/Source/TwoSisters/Hairpin_node_normal.png']
     const node3 = ['image/Record/Source/TwoSisters/Hongryeon_node_glow.png', 'image/Record/Source/TwoSisters/Hongryeon_node_normal.png']
@@ -24,41 +28,21 @@ export default function Record(){
     const infoNode4 = ['image/Record/UI/information_04_glow.png', 'image/Record/UI/information_04_normal.png'];
     const infoNode5 = ['image/Record/UI/information_05_glow.png', 'image/Record/UI/information_05_normal.png'];
     
+    // 홍련은 시작부터 있으므로 1부터 시작
+    const nodeIndex = [0, 1, 0, 0, 0, 0, 0, 0, 0];
+
     // 노드 획득 시 이미지 변경(pre->glow), 이름표 추가, 선 추가
     // 획득한 노드 획득 시 이미지 변경(glow->normal)
     // const [sessionValue, setSessionValue] = useState(true);
     // const [currentImageIndex, setCurrentImageIndex] = useState(0);
   
-    // const getMainNodes = (have) => {
-    //   if (have === true) {
-    //     setSessionValue(true);
-    //   } else {
-    //     setSessionValue(false);
+    // const [message, setMessage] = useState('');
+    // const clickPreNode  = (num) => {
+    //   if (num === 0) {
+    //     setMessage('획득할 수 있습니다');
     //   }
     // };
   
-    // const handleClickImage = () => {
-    //   if (sessionValue) {
-    //     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % 3);
-    //   } 
-    // };
-  
-
-    // json data 불러오기
-    const [data, setData] = useState(null);
-
-    // useEffect(() => {
-    //   axios.get('/item/getItems/2')
-    //     .then(response => {
-    //       console.log(res.data)
-    //       setData({"name": res.data.name, "kind": res.data.kind,
-    //       "id": res.data.id, "script": res.data.chapter
-    //     });
-    //     })
-    //     .catch(error => {
-    //       console.log(error);
-    //     });
-    // }, []);    
     return (
         
       <div>
@@ -67,13 +51,30 @@ export default function Record(){
         </div>
         
         <div>
-        <img src={node1[currentImageIndex]} style={{ position: "absolute", top:"988px", left:"1953px"}} onClick={handleClickImage} />
-        <button onClick={() => getMainNodes('have')}></button>
-        </div>
-        <div>
-        <img src={node2[currentImageIndex]} style={{ position: "absolute", top:"413px", left:"119px"}} onClick={handleClickImage} />
-        <button onClick={() => getMainNodes('have')}></button>
+        <img src={node1[nodeIndex[0]]} style={{ position: "absolute", top:"988px", left:"1953px"}} onClick={handleClickImage} />
+        <button onClick={() => visitMainNode(3, 7) && clickPreNode(nodeIndex[0])}></button>
+          <div className="node1_behind">
+          <img src={nameUI} className="name_ui_1"></img>
+          <p className="name_1">사체 부검결과서</p>
 
+          <img src={infoNode3[0]} className="node1_info"></img>
+          <img src={subNode[0]} className="node1_1" onClick={handleClickImage} />
+          <button onClick={() => visitNode(3, 7_1)}></button>
+          </div></div>
+
+        <div>
+        <img src={node2[nodeIndex[1]]} style={{ position: "absolute", top:"413px", left:"119px"}} onClick={handleClickImage} />
+        <button onClick={() => visitMainNode(3, 7) && clickPreNode(nodeIndex[0])}></button>
+          <img src={nameUI} className="name_ui_1"></img>
+          <p className="name_1">사체 부검결과서</p>
+
+          <img src={infoNode3[0]} className="node1_info"></img>
+          <div className="node2_behind">
+          <img src={subNode[0]} className="node1_1" onClick={handleClickImage} />
+          <button onClick={() => visitNode(3, 7_1)}></button>
+          </div></div>
+
+        <div>
         <img src={node3[currentImageIndex]} style={{ position: "absolute", top:"184px", left:"896px"}} onClick={handleClickImage} />
         <button onClick={() => getMainNodes('have')}></button>
 
