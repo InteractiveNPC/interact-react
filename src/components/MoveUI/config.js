b7c76ab96c4cb2e1bd64a244434a440940e5450e
HeoJiye <heoze1031@gmail.com>
2023-06-06 18:33:27 +0900

    증거 획득 시 미니게임 삭제 & Record hook example 수정

--- src/components/MoveUI/config.js
+++ src/components/MoveUI/config.js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import woodcutterLocs from "./woodcutter";
 import twosistersLocs from "./twosisters";
 
@@ -10,3 +13,45 @@ export const locations = {
   1: woodcutterLocs,
   3: twosistersLocs,
 };
+
+export const useHaveItem = (itemName) => {
+  const [item, setItem] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      let have;
+      if(itemName === "선녀옷") {
+        have = await session_item(1, 6);
+      }
+      if(itemName === "부검서") {
+        have = await session_item(3, 7);
+      }
+      if(itemName === "비녀") {
+        have = await session_item(3, 8);
+      }
+      setItem(!have);
+    })();
+  })
+
+  return item;
+};
+
+const session_item = (chapter, item_id) => {
+  return new Promise((resolve) =>
+    axios
+      .get(`/meet/${chapter}`)
+      .then(({ data }) => {
+        for(let id of data) {
+          if(id == item_id) {
+            resolve(true);
+            return;
+          }
+        }
+        resolve(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        resolve();
+      })
+  );
+} 
