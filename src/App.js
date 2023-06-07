6f9f7ff3308145dcf6230f458d64f2774cc5de55
karam Choi <86296569+karamChoi2523@users.noreply.github.com>
2023-06-08 02:44:09 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ export default () => {
    return (
       <div id="App">
         <ContextProvider volumeData={[volume, setVolume]} chapterData={[chapter, setChapter]}>
-        <Loading App={<Document chapter={3} />} />
+        <Loading App={<Intro />} />
         </ContextProvider>
         <audio id="bgm" loop={true} autoPlay={true} volume={volume[0]}/>
         <div id="effect" className={volume[1]}/>
